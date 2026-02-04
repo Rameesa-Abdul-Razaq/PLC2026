e4c8eecf74ed13c1df396784796a9494c80652d1
Rameesa-Abdul-Razaq <rameesa.abdulrazaq@gmail.com>
2026-02-04 17:23:43 +0000

    task 3 completed

--- lab2-functionalPrimer/haskell/task3.hs
+++ lab2-functionalPrimer/haskell/task3.hs
@@ -1,9 +1,11 @@
+--this function returns -1 for negative numbers, 0 for zero, and 1 for positive numbers
 sgn x = if x < 0 
           then -1 
           else if x == 0 
               then 0 
               else 1
-
+--alternative implementation using guards
+-- this is the guard function version 
 sgnGuards x | x < 0 = -1
             | x == 0 = 0
             | otherwise = 1
