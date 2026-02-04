c1f5a731f3b9fb1c9ef8c9dd583883abab8a1d52
Rameesa-Abdul-Razaq <rameesa.abdulrazaq@gmail.com>
2026-02-04 17:32:24 +0000

    Task 4 complete

--- lab2-functionalPrimer/haskell/task4.hs
+++ lab2-functionalPrimer/haskell/task4.hs
@@ -4,9 +4,13 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
-
+    then ask (prompt ++ "!") -- recursion here to loop again with modified prompt
+    else if (line == "quit") then putStrLn("quitting") -- so this line is added for a loop
+    else do
+      putStrLn ("you said: " ++ reverse line)
+      ask prompt -- them recursion here to loop again by calling the method
+  -- cuurently does not loop and only asks once
+  -- to loop, we can use recursion, by calling the method within itself
 main :: IO ()
 main =
   do
