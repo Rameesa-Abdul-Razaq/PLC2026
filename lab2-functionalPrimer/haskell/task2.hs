690dc086fbcc4bb29e8ac172f174c0924450783d
Rameesa-Abdul-Razaq <rameesa.abdulrazaq@gmail.com>
2026-02-04 17:19:54 +0000

    task 2 complete

--- lab2-functionalPrimer/haskell/task2.hs
+++ lab2-functionalPrimer/haskell/task2.hs
@@ -1,4 +1,10 @@
+-- the onePlusone function should print "1+1=2" to the screen
+
+
+onePlusone = putStrLn("1+1=2")
 main = do
     putStrLn "Welcome to the programme. Please enter your name"
     name <- getLine
     putStrLn("Hello " ++ name ++ ", hope you like Haskell.")
+    onePlusone
+    --the do block must be completed, putStrLn and getLine are IO actions
