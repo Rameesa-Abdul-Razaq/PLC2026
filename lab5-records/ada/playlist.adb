3ae9714847d7c096a63e6ddce66dfc1d26baff07
Rameesa-Abdul-Razaq <rameesa.abdulrazaq@gmail.com>
2026-02-18 13:05:52 +0000

    Lab 5 complete

--- lab5-records/ada/playlist.adb
+++ lab5-records/ada/playlist.adb
@@ -3,48 +3,71 @@ with Ada.Float_Text_IO;     use Ada.Float_Text_IO;
 with Ada.Text_IO;           use Ada.Text_IO;
 with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
-procedure PlayList is
+procedure PlayList is -- defines a subprogram named PlayList, which is the main entry point of the program
 
-    type Person is record
+    type Person is record -- this defines a record type named Person
+                          -- which has a single field called name of type Unbounded_String
         name : Unbounded_String;
-    end record;
+    end record; -- end record
 
-    procedure Put_Person (p : Person) is
+    procedure Put_Person (p : Person) is -- nested procedure to print the name of a Person
     begin
-        Put (To_String (p.name));
+        Put (To_String (p.name)); -- takes person as an input
     end Put_Person;
 
-    type Item is record
-        name        : Unbounded_String;
-        performer   : Person;
-        length_secs : Float;
-    end record;
+    --type Item is record -- another record type with necessary fields to represent a music item
+    --    name        : Unbounded_String;
+    --    performer   : Person;
+    --    length_secs : Float;
+   -- end record;
 
-    procedure Put_Item (i : Item) is
-    begin
-        Put (To_String (i.name));
-        Put (" by ");
-        Put_Person (i.performer);
-        Put (" (");
-        Put (i.length_secs, aft => 1, exp => 0);
-        Put ("s)");
-    end Put_Item;
+      -- union in ada for item to put peice and pause togehter
+      type item_variant_type is (piece, pause); 
+    type Item(item_variant : item_variant_type) is 
+         record 
+            length_secs : Float; 
+            case item_variant is 
+               when piece => name : Unbounded_String; 
+                     performer : Person; 
+               when pause => 
+                     null; 
+            end case; 
+         end record; 
 
-    piece1 : Item :=
+    procedure Put_Item (i : Item) is -- printing to the terminal the details of an Item, 
+                                     --including its name, performer, and length in seconds
+    begin 
+      case i.item_variant is 
+         when Piece => 
+            Put (To_String (i.name)); 
+            Put (" by "); 
+            Put_Person (i.performer); 
+            Put (" ("); 
+            Put (i.length_secs, aft => 1, exp => 0); 
+            Put ("s)"); 
+         when Pause => 
+            Put ("Pause ("); 
+            Put (i.length_secs, aft => 1, exp => 0); 
+            Put ("s)"); 
+      end case; 
+   end Put_Item; 
+
+    -- consutructing an instance of the Item record type to represent a music piece,
+    -- with the name "Moonlight Sonata", performed by Claudio Arrau, and a length
+    piece1 : Item(item_variant => piece) :=
        (name => To_Unbounded_String ("Moonlight Sonata"),
         performer => (name => To_Unbounded_String ("Claudio Arrau")),
         length_secs => 17.0*60.0+26.0
        );
 
-    -- pause1 : Item :=
-    --    (
-    --     item_variant => PAUSE,
-    --     length_secs => 5.0
-    --    );
+     pause1 : Item(item_variant => pause) :=
+        (
+         length_secs => 5.0
+       );
 
 begin
     Put_Item (piece1);
     Put_Line ("");
-    -- Put_Item(pause1);
-    -- Put_Line ("");
+    Put_Item(pause1);
+    Put_Line ("");
 end PlayList;
--- lab5-records/haskell/playlist.hs
+++ lab5-records/haskell/playlist.hs
@@ -4,34 +4,48 @@ import Text.Printf
 
 -- start of code to focus on in Practical 4
 
-data Person
+data Person -- another record
     = Person
         {
           person_name :: String
         }
-    deriving (Eq)
+    deriving (Eq) -- automatically allows comparisons, so item1 == item2 can only work if this is here
 
 instance Show Person where 
     -- using Java terminology: Person implements interface Show
     show (Person name) = name -- how to format a Person record
 
-data Item
-    = Piece
+data Item -- data is a record type, and we are creating a new one called Item
+    = Piece -- this is the one constructor 
         {
+            -- what the record contains
             item_name :: String,
             item_performer :: Person,
             item_length_secs :: Float
         }
+        | -- union of 2 records peice and pause
+      Pause 
+        {item_length_secs:: Float
+        }
     deriving (Eq)
 
+--defines how an item should be printed
 instance (Show Item) where
     show (Piece name performer len) =
         printf "%s by %s (%.1fs)" name (show performer) len
+        -- pattern matching to the constructor Piece, and then using the fields to format the string
+        -- %s is the string matching
+        --1fs is the float matching, and .1 means to round to 1 decimal place
+    show (Pause len) =
+        printf "Pause (%.1fs)" len
+        -- show the pause1 as "Pause (5.0s)"
 
+-- the construction of the records, using the constructor Piece and Pause, 
+--and then filling in the fields with the appropriate values
 piece1 =
     Piece
     { 
-        item_name = "Moonlight Sonata",
+        item_name = "Prelude",
         item_performer = Person "Claudio Arrau",
         item_length_secs = 17*60+26
     }
@@ -43,29 +57,36 @@ piece2 =
         item_performer = Person "Daniel Barenboim",
         item_length_secs = 16*60+49
     }
-  
-{-
+
 pause1 =
     Pause
     { 
         item_length_secs = 5
     }
--}
 
-main =
-    do
-    -- putStrLn "piece1 and piece2 sorted by length:"
-    -- putStrLn $ show shorterPiece
-    -- putStrLn $ show longerPiece
-    putStr "piece1 = "
-    putStrLn $ show piece1
---    putStr "pause1 = "
---    putStrLn $ show pause1
-
--- ... = sortTwoItems (piece1, piece2) -- TASK
+--(shorterPiece, longerPiece) = sortTwoItems (piece1, piece2) -- TASK 
+-- combining both pieces into a tuple, 
+--and then using the function sortTwoItems to sort them by length,
+-- and then unpacking the sorted tuple into shorterPiece and longerPiece
 
 sortTwoItems (item1, item2) = 
     if item_length_secs item1 <= item_length_secs item2
         then (item1, item2)
         else (item2, item1)
+
+main =
+    do
+        let (shorterPiece, longerPiece) = sortTwoItems (piece1, piece2)
+        -- just showing the terminal for the user
+        putStrLn "piece1 and piece2 sorted by length:" 
+        putStrLn $ show shorterPiece
+        putStrLn $ show longerPiece
+        putStr "piece1 = "
+        putStrLn $ show piece1
+        putStr "pause1 = "
+        putStrLn $ show pause1
+
+
+
+
 
